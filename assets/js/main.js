3b147121a1d5a1e3484ee3776f53bcd78fbdee26
diegopaolantonio <dpaolantonio.expertoing@gmail.com>
2022-10-25 21:09:32 -0300

    Commit 2, agregue local storage, event, y control del html desde el js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,241 +1,266 @@
-alert("Cotizador de costo mensual de mano de obra de EXPERTO INGENIERIA");
+var costoTotal = 0;
+var precioHora = 0;
+var precioHoraHerr = 0;
 
-let seguir = true;
+var cotizacion = {
+  id: Date,
+  referencia: "",
+  trabajo: "",
+  herramientas: "no",
+  personas: 0,
+  dias: 0,
+  horas: 0,
+  costoTotal: 0,
+};
 
-function condiciones(
-  trabajo,
-  cantidad,
-  opcionTiempo,
-  dias,
-  precio,
-  herramientas,
-  costo,
-  horas,
-  trabajoString,
-  detalle
-) {
-  this.trabajo = trabajo; // Tipo de personal que se necesita
-  this.cantidad = cantidad; // El dato de tiempo va en dias o meses
-  this.opcionTiempo = opcionTiempo; // Dias o meses
-  this.dias = dias; // Cantidad de dias que se trabaja
-  this.precio = precio; // Precio de la hora
-  this.herramientas = herramientas; // Necesita o no herramientas
-  this.costo = costo; // Costo total del servicio
-  this.horas = horas; // Horas de jornada laboral
-  this.trabajoString = trabajoString; // String del tipo de trabajo
-  this.detalle = detalle; // Detalle del trabajo
-}
+var cotizacionesRealizadas = [];
 
-var cantidadConsultas = [0,0,0];
-
-var trabajoProgramador = new condiciones(0, 0, 0, 0, 0, 0, 0, 0, "Indefinido");
-var trabajoElectricista = new condiciones(0, 0, 0, 0, 0, 0, 0, 0, "Indefinido");
-var trabajoAsistencia = new condiciones(0, 0, 0, 0, 0, 0, 0, 0, "Indefinido");
-var trabajoGeneral = new condiciones(0, 0, 0, 0, 0, 0, 0, 0, "Indefinido");
-var trabajoTotal = new condiciones(0, 0, 0, 0, 0, 0, 0, 0, "Indefinido");
-var trabajoTotalCosto = 0;
-
-var dato = 0; // opcion ingresada
-var min = 0; // valor minimo para la opcion
-var max = 0; // valor maximo para la opcion
-var repetir = true; // booleano para repetir la peticion de informacion
-var consultaSiNo = "";
-var otraObra = "";
-var otraObraSi = false;
-
-function costoHora(trabajo) {
-  if (trabajo == 1) {
-    trabajoGeneral.precio = 20;
-    trabajoGeneral.trabajoString = "Programador";
-  } else if (trabajo == 2) {
-    trabajoGeneral.precio = 15;
-    trabajoGeneral.trabajoString = "Electricista";
-  } else if (trabajo == 3) {
-    trabajoGeneral.precio = 14;
-    trabajoGeneral.trabajoString = "Asistencia Tecnica";
-  } else if (trabajo == 4) {
-    trabajoGeneral.precio = 0;
-    trabajoGeneral.trabajoString = "Indefinido";
+function costoHora(val) {
+  let precio = 0;
+  if (val == "Programador") {
+    console.log(1);
+    precio = 20;
+  } else if (val == "Electricista") {
+    console.log(2);
+    precio = 15;
+  } else if (val == "Asistencia Tecnica") {
+    console.log(3);
+    precio = 14;
   }
 
-  return trabajoGeneral.precio;
+  return precio;
 }
 
-function controlarNumero(dato, min = 0, max = 0) {
-  if (isNaN(dato) || dato == 0) {
-    repetir = true;
-  } else if ((dato < min || dato > max) && min != 0 && max != 0) {
-    repetir = true;
-  } else {
-    repetir = false;
-  }
-  return repetir;
+function sincronizarConLocalStorage() {
+  localStorage.setItem("cotizacion", JSON.stringify(cotizacionesRealizadas));
 }
 
-function formatearSiNo(entradaSiNo) {
-  if (
-    entradaSiNo == "Si" ||
-    entradaSiNo == "si" ||
-    entradaSiNo == "SI" ||
-    entradaSiNo == "sI"
-  ) {
-    entradaSiNo = "Si";
-  } else if (
-    entradaSiNo == "no" ||
-    entradaSiNo == "No" ||
-    entradaSiNo == "NO" ||
-    entradaSiNo == "nO"
-  ) {
-    entradaSiNo = "No";
-  }
-  return entradaSiNo;
-}
+//if (typeof window === "object") {
+window.addEventListener("DOMContentLoaded", function () {
+  // NUEVO: Contenido cargado
+  console.log("EL DOM SE CARGO");
 
-function calculoTotal(programador, electricista, asistencia, general) {
-  let total = new condiciones(0, 0, 0, 0, 0, 0, 0, 0, "Indefinido");
+  cotizacionesRealizadas = JSON.parse(localStorage.getItem("cotizacion")) || [];
+  agregarCotizacion();
 
-  total.costo = programador.costo + electricista.costo + asistencia.costo;
-  total.detalle = general.detalle;
-
-  return total;
-}
-
-trabajoGeneral.detalle = String(
-  prompt("Descripcion del trabajo o codigo de referencia")
-);
-
-do {
-  do {
-    trabajoGeneral.trabajo = Number(
-      prompt(
-        "La obra '" +
-          trabajoGeneral.detalle +
-          "' necesita: 1-Programador, 2-Electricista, 3-Asistencia tecnica, 4-Finalizar",
-        0
-      )
-    );
-  } while (controlarNumero(trabajoGeneral.trabajo, 1, 4));
-
-  trabajoGeneral.precio = costoHora(trabajoGeneral.trabajo);
-
-  do {
-    if (trabajoGeneral.trabajo == 1) {
-      trabajoGeneral.herramientas = String(
-        prompt("Llevar computadora propia?")
-      );
-    } else if (trabajoGeneral.trabajo == 2) {
-      trabajoGeneral.herramientas = String(
-        prompt("Llevar herramientas propias?")
-      );
-    } else if (trabajoGeneral.trabajo == 3) {
-      alert("No se asiste con herramientas ni computadora");
-      trabajoGeneral.herramientas = "no";
+  const formatearSiNo = (val) => {
+    if (val == "si" || val == "SI" || val == "sI" || val == "Si") {
+      val = "Si";
+      console.log("Si");
+    } else if (val == "no" || val == "No" || val == "NO" || val == "nO") {
+      val = "No";
+      console.log("No");
     }
-
-    trabajoGeneral.herramientas = formatearSiNo(trabajoGeneral.herramientas);
-  } while (
-    trabajoGeneral.herramientas != "Si" &&
-    trabajoGeneral.herramientas != "No"
-  );
-
-  if (trabajoGeneral.herramientas == "Si") {
-    trabajoGeneral.precio = trabajoGeneral.precio * 1.25;
-  }
-
-  if (trabajoGeneral.trabajo != 4) {
-    do {
-      trabajoGeneral.cantidad = Number(
-        prompt("Cantidad de personas que necesita: ", 0)
-      );
-    } while (controlarNumero(trabajoGeneral.cantidad));
-    do {
-      trabajoGeneral.opcionTiempo = Number(
-        prompt("Tiempo que necesitara es en: 1-dias, 2-meses", 0)
-      );
-    } while (controlarNumero(trabajoGeneral.opcionTiempo, 1, 2));
-
-    if (trabajoGeneral.opcionTiempo == 1) {
-      do {
-        trabajoGeneral.dias = Number(
-          prompt("Cantidad de dias que necesitara el personal: ", 0)
-        );
-      } while (controlarNumero(trabajoGeneral.dias));
-    } else if (trabajoGeneral.opcionTiempo == 2) {
-      do {
-        trabajoGeneral.dias = Number(
-          prompt("Cantidad de meses que necesitara el personal: ", 0)
-        );
-      } while (controlarNumero(trabajoGeneral.dias));
-
-      trabajoGeneral.dias = trabajoGeneral.dias * 24;
+    return val;
+  };
+  
+  const checkString = (val) => {
+    if (/[a-zA-z]+$/.test(val) && val.length >= 1) {
+      return true;
     }
+    return false;
+  };
+  
+  const checkNumero = (val) => {
+    let number = parseInt(val);
+  
+    if (/[^0-9]/g.test(number) && typeof number === "number") {
+      return false;
+    }
+    return true;
+  };
 
-    do {
-      trabajoGeneral.horas = Number(
-        prompt("Horas diarias de la jornada laboral promedio", 0)
+  // Validamos el campo referencia
+  referencia.addEventListener("input", (e) => {
+    let validaReferencia = checkString(e.target.value);
+    if (validaReferencia) {
+      error_referencia.style.display = "none";
+      error_referencia.innerHTML = ``;
+    } else {
+      error_referencia.style.display = "block";
+      error_referencia.innerHTML = `Debe ingresar numeros o letras`;
+    }
+  });
+
+  // Validamos el campo trabajo
+  trabajo.addEventListener("input", (e) => {
+    let validaTrabajo = checkString(e.target.value);
+    precioHora = costoHora(e.target.value);
+    if (validaTrabajo) {
+      error_trabajo.style.display = "none";
+      error_trabajo.innerHTML = ``;
+    } else {
+      error_trabajo.style.display = "block";
+      error_trabajo.innerHTML = `Debe ingresar alguna opcion`;
+    }
+  });
+
+  // Validamos el campo herramientas
+  herramientas.addEventListener("input", (e) => {
+    let validaHerramientas = checkString(e.target.value);
+    validaHerramientas = formatearSiNo(e.target.value);
+    if (validaHerramientas == "Si") {
+      precioHoraHerr = 1.25;
+      error_herramientas.style.display = "none";
+      error_herramientas.innerHTML = ``;
+    } else {
+      precioHoraHerr = 1;
+      error_herramientas.style.display = "block";
+      error_herramientas.innerHTML = `Debe ingresar alguna opcion`;
+    }
+  });
+
+  // Validamos el campo personas
+  personas.addEventListener("input", (e) => {
+    let validaPersonas = checkNumero(e.target.value);
+    if (validaPersonas) {
+      error_personas.style.display = "none";
+      error_personas.innerHTML = ``;
+    } else {
+      error_personas.style.display = "block";
+      error_personas.innerHTML = `Debe ingresar sólo numeros`;
+    }
+  });
+
+  // Validamos el campo dias
+  dias.addEventListener("input", (e) => {
+    let validaDias = checkNumero(e.target.value);
+    if (validaDias) {
+      error_dias.style.display = "none";
+      error_dias.innerHTML = ``;
+    } else {
+      error_dias.style.display = "block";
+      error_dias.innerHTML = `Debe ingresar sólo numeros`;
+    }
+  });
+
+  // Validamos el campo horas
+  horas.addEventListener("input", (e) => {
+    let validaHoras = checkNumero(e.target.value);
+    if (validaHoras) {
+      error_horas.style.display = "none";
+      error_horas.innerHTML = ``;
+    } else {
+      error_horas.style.display = "block";
+      error_horas.innerHTML = `Debe ingresar solo numeros`;
+    }
+  });
+
+  // Seleccionamos el formulario
+  let formulario = document.querySelector("#formulario");
+  // mensaje con los datos del formulario
+  let mensajeDatos = document.querySelector("#alerta");
+  // escondemos el mensaje
+  mensajeDatos.style.display = "none";
+
+  // Input de error
+  let error_referencia = document.querySelector("#error_referencia");
+  let error_trabajo = document.querySelector("#error_trabajo");
+  let error_herramientas = document.querySelector("#error_herramientas");
+  let error_personas = document.querySelector("#error_personas");
+  let error_dias = document.querySelector("#error_dias");
+  let error_horas = document.querySelector("#error_horas");
+
+  error_referencia.style.display = "none";
+  error_trabajo.style.display = "none";
+  error_herramientas.style.display = "none";
+  error_personas.style.display = "none";
+  error_dias.style.display = "none";
+  error_horas.style.display = "none";
+
+  formulario.addEventListener("submit", function (e) {
+    e.preventDefault();
+    // guardamos datos que ingresamos en los input
+    let referencia = document.querySelector("#referencia").value;
+    let trabajo = document.querySelector("#trabajo").value;
+    let herramientas = document.querySelector("#herramientas").value;
+    let personas = document.querySelector("#personas").value;
+    let dias = document.querySelector("#dias").value;
+    let horas = document.querySelector("#horas").value;
+
+    if (referencia && trabajo && herramientas && personas && dias && horas) {
+      let infoReferencia = document.querySelector("#infoReferencia"),
+        infoTrabajo = document.querySelector("#infoTrabajo"),
+        infoHerramientas = document.querySelector("#infoHerramientas"),
+        infoPersonas = document.querySelector("#infoPersonas"),
+        infoDias = document.querySelector("#infoDias"),
+        infoHoras = document.querySelector("#infoHoras"),
+        infoCostoTotal = document.querySelector("#infoCostoTotal");
+
+      costoTotal = dias * precioHora * precioHoraHerr * personas * horas;
+      console.log(costoTotal);
+      console.log(precioHoraHerr);
+
+      cotizacion.id = Date;
+      cotizacion.referencia = referencia;
+      cotizacion.trabajo = trabajo;
+      cotizacion.herramientas = herramientas;
+      cotizacion.personas = personas;
+      cotizacion.dias = dias;
+      cotizacion.horas = horas;
+      cotizacion.costoTotal = costoTotal;
+
+      let cotizaciones = [];
+
+      cotizaciones.push(cotizacion);
+
+      cotizacionesRealizadas = [...cotizacionesRealizadas, ...cotizaciones];
+
+      //      document.location.reload ();
+
+      infoReferencia.innerHTML = `${cotizacion.referencia}`;
+      infoTrabajo.innerHTML = `${cotizacion.trabajo}`;
+      infoHerramientas.innerHTML = `${cotizacion.herramientas}`;
+      infoPersonas.innerHTML = `${cotizacion.personas}`;
+      infoDias.innerHTML = `${cotizacion.dias}`;
+      infoHoras.innerHTML = `${cotizacion.horas}`;
+      infoCostoTotal.innerHTML = `${cotizacion.costoTotal}`;
+      //      mensajeDatos.style.display = "block";
+
+      agregarCotizacion();
+
+      console.log(cotizacion);
+      console.log(cotizaciones);
+      console.log(cotizacionesRealizadas);
+    } else {
+      alert("Debe llenar los campos");
+    }
+  });
+
+});
+
+function agregarCotizacion() {
+  tabla.innerHTML = ``;
+  cotizacionesRealizadas.forEach((element, index) => {
+    tabla.innerHTML += `<div class="tablaDos">
+             <tr>
+             <td class="text-center">${element.referencia}"</td>
+             <td class="text-center">${element.id}"</td>
+             <td class="text-center">${element.trabajo}</td>
+             <td class="text-center">${element.costoTotal}</td>
+             <td class="text-center"><a href="#" id="${element.id}" class="borrar-cotizacion" data-id="1">X</a></td>
+           </tr>
+           </div>`;
+  });
+
+  // Seleccionamos el boton eliminar
+  let buttonDelete = document.querySelectorAll(".borrar-cotizacion");
+
+  // Lo recorremos
+  buttonDelete.forEach((element) => {
+    element.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      let id = e.target.id;
+      console.log(id);
+      cotizacionesRealizadas = cotizacionesRealizadas.filter(
+        (elemento) => elemento.id != id
       );
-    } while (controlarNumero(trabajoGeneral.horas));
 
-    trabajoGeneral.costo =
-      trabajoGeneral.dias *
-      trabajoGeneral.precio *
-      trabajoGeneral.cantidad *
-      trabajoGeneral.horas;
+      console.log("Salida de cotizacionesRealizadas", cotizacionesRealizadas);
+      agregarCotizacion();
+    });
+  });
 
-    alert("Costo total: USD " + trabajoGeneral.costo);
-  }
-
-  if (trabajoGeneral.trabajo != 4) {
-    console.log("Costo total " + trabajoGeneral.costo);
-
-    document.write(
-      "<p>El costo total de " +
-        trabajoGeneral.trabajoString +
-        " es de: USD " +
-        trabajoGeneral.costo +
-        "</p>"
-    );
-  }
-
-  if (trabajoGeneral.trabajo === 1) {
-    trabajoProgramador.costo = trabajoProgramador.costo + trabajoGeneral.costo;
-    cantidadConsultas[0]++;
-  } else if (trabajoGeneral.trabajo === 2) {
-    trabajoElectricista.costo =
-      trabajoElectricista.costo + trabajoGeneral.costo;
-      cantidadConsultas[1]++;
-    } else if (trabajoGeneral.trabajo === 3) {
-    trabajoAsistencia.costo = trabajoAsistencia.costo + trabajoGeneral.costo;
-    cantidadConsultas[2]++;
-  }
-
-  do {
-    otraObra = prompt(String("Desea agregar mas personal a la cotizacion?"));
-
-    otraObra = formatearSiNo(otraObra);
-  } while (otraObra != "Si" && otraObra != "No");
-
-  if (otraObra == "Si") {
-    otraObraSi = true;
-  } else if (otraObra == "No") {
-    otraObraSi = false;
-  }
-} while (otraObraSi);
-
-trabajoTotal = calculoTotal(
-  trabajoProgramador,
-  trabajoElectricista,
-  trabajoAsistencia,
-  trabajoGeneral
-);
-
-console.log("Codigo de obra: " + trabajoTotal.detalle);
-console.log("Costo total " + trabajoTotal.costo);
-console.log("Cantidad de veces que se agrego Programadores:", cantidadConsultas[0]);
-console.log("Cantidad de veces que se agrego Electricistas:", cantidadConsultas[1]);
-console.log("Cantidad de veces que se agrego Asistencia tecnica:", cantidadConsultas[2]);
-
-
-document.write("<h2>Resultado:</h2>");
-document.write("<h3>Codigo de obra: " + trabajoTotal.detalle + "</h3>");
-document.write("<h4>El costo total es de: USD " + trabajoTotal.costo + "</h4>");
+  sincronizarConLocalStorage();
+}
